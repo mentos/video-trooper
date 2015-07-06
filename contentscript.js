ed8b371e7690cab1a2e9f55142a80705b1627dfa
Ioannis Tziligkakis <gtsiligkakis@gmail.com>
2015-07-06 07:47:03 +0300

    Updated to version 0.0.2
    * Display page action icon only if videos available
    * Replace TVXS.gr video player with Youtube's embeded player

--- contentscript.js
+++ contentscript.js
@@ -1,44 +1,53 @@
+var youtubeVideosIds = []
+  , getYoutubeVideoId;
+
+getYoutubeVideoId = function(url) {
+  var regex = new RegExp("[\\?&]v=([^&#]*)")
+    , results
+    , parser;
+
+  parser = document.createElement("a");
+  parser.href = url;
+
+  results = regex.exec(parser.search);
+
+  if (results.length) {
+    return decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+};
+
+$('div[id*=jwplayer][id*=-div] param[name=flashvars]').each(function(index, node) {
+  var params = node.value.split('&');
+  for (var i = 0; i < params.length; i++) {
+    var pair = params[i].split('=')
+      , paramName  = pair[0]
+      , paramValue = pair[1];
+
+    if (paramName == 'file' && paramValue.match(/youtube/)) {
+      var url = decodeURIComponent(paramValue.replace(/\+/g,  " "))
+        , id = getYoutubeVideoId(url)
+        , iframe = document.createElement('iframe');
+
+      iframe.width = 463;
+      iframe.height = 280;
+      iframe.src = 'https://www.youtube.com/embed/' + id;
+      iframe.frameBorder = 0;
+      iframe.allowFullscreen = true;
+
+      $(node).parents('[id*=jwplayer][id*=-div]').replaceWith($(iframe));
+      youtubeVideosIds.push(id);
+    }
+  }
+});
+
 /* Send message to background script in order to disaply action icon */
-chrome.runtime.sendMessage({ from: 'content', subject: 'showPageAction' });
+if (youtubeVideosIds.length) {
+  chrome.runtime.sendMessage({ from: 'content', subject: 'showPageAction' });
+}
 
 /* Handle "ping" sent from popup script */
-chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
   if ((msg.from === 'popup') && (msg.subject === 'getVideoIds')) {
-    var youtubeVideosIds = []
-      , getYoutubeVideoId;
-
-    getYoutubeVideoId = function(url) {
-      var regex = new RegExp("[\\?&]v=([^&#]*)")
-        , results
-        , parser;
-
-      parser = document.createElement("a");
-      parser.href = url;
-
-      results = regex.exec(parser.search);
-
-      if (results.length) {
-        return decodeURIComponent(results[1].replace(/\+/g, " "));
-      }
-    };
-
-    $('div[id*=jwplayer][id*=-div] param[name=flashvars]').each(function(index, node) {
-      var params = node.value.split('&');
-
-      for (var i = 0; i < params.length; i++) {
-        var pair = params[i].split('=')
-          , paramName  = pair[0]
-          , paramValue = pair[1];
-
-        if (paramName == 'file' && paramValue.match(/youtube/)) {
-          var url = decodeURIComponent(paramValue.replace(/\+/g,  " "))
-            , id = getYoutubeVideoId(url);
-
-          youtubeVideosIds.push(id);
-        }
-      }
-    });
-
     response(youtubeVideosIds);
   }
 });
